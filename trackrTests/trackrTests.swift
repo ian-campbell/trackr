c91030e3837c797f2825ad2b279e7efe8ba901dd
Ian Campbell <ian.campbell87@gmail.com>
2019-10-12 17:09:45 -0400

    Initial Commit

---
+++ trackrTests/trackrTests.swift
@@ -0,0 +1,34 @@
+//
+//  trackrTests.swift
+//  trackrTests
+//
+//  Created by ian campbell on 10/12/19.
+//  Copyright © 2019 campbell. All rights reserved.
+//
+
+import XCTest
+@testable import trackr
+
+class trackrTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
