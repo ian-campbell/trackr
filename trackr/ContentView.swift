f12545b1d6e4304dec9990021485ddf63a13faaa
Ian Campbell <ian.campbell87@gmail.com>
2019-10-12 18:59:49 -0400

    Initial commit

--- trackr/ContentView.swift
+++ trackr/ContentView.swift
@@ -11,8 +11,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
